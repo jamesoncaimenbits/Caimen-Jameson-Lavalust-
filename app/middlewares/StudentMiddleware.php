0057259ca5e62b7d64ceaf3e96a92f879ba57c47
jameson.caimenbits <jamesonramoscaimen@gmail.com>
2026-08-18 17:55:57 +0800

    Add by Jameson

---
+++ app/middlewares/StudentMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+
+class StudentMiddleware extends Middleware
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        session_start();
+
+        if (!isset($_SESSION['student_access']) || $_SESSION['student_access'] !== true) {
+            
+
+            redirect('student');
+            exit;
+        }
+    }
+}
